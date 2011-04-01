2d97a25b7dc42147625ac28b7fb211cab29beaa3
Robert Spitzenpfeil <robert@linuxbox.site>
2011-04-02 01:04:53 +0200

    Forgot to update 'flash.sh'. Fixed.

--- flash.sh
+++ flash.sh
@@ -7,5 +7,5 @@
 #
 
 avrdude -c usbtiny -p atmega168 -P usb -b 115200 -e -B 100 -U lock:w:0x3F:m -U lfuse:w:0xFF:m -U hfuse:w:0xDD:m -U efuse:w:0x00:m
-avrdude -c usbtiny -p atmega168 -B 1 -P usb -b 115200 -U flash:w:V3_x_boards_test__plus__ATmegaBOOT_168_diecimila.hex -U lock:w:0x0F:m
+avrdude -c usbtiny -p atmega168 -B 1 -P usb -b 115200 -U flash:w:V3_x_boards_test__plus__optiboot_diecimila.hex -U lock:w:0x0F:m
 
